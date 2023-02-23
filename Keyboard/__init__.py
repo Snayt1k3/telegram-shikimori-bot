374ed2d23c3f7509043b0d4e1538de5444b9c4d7
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-02-23 17:25:29 +0300

    add keyboards

---
+++ Keyboard/__init__.py
@@ -0,0 +1,1 @@
+from Keyboards.keyboard import *
---
+++ Keyboard/keyboard.py
@@ -0,0 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+keyboard_status = ReplyKeyboardMarkup()
+keyboard_status.add("completed").add("watching").add("planned").add("rewatching").add("dropped")
+
+
+default_keyboard = ReplyKeyboardMarkup()
+keyboard_status.add("/AnimeSearch").add("/AnimeMark")
+
+
+keyboard_cancel = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_cancel.add('Cancel')
