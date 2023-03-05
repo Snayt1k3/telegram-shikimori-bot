97519071dff14e9e9b87218d56c50fe9c963c965
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-03-05 12:39:01 +0300

    refactor

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
+import os
+
+
 headers = {
     'User-Agent': 'Snayt1k3-API',
-    'Authorization': "Bearer " + os.environ.get("SHIKI_TOKEN", 'Y69f-NA5ESOm6KTy4-lcpKrJMsGhvyFMVebRNZuO7pk')
+    'Authorization': "Bearer " + os.environ.get("SHIKI_TOKEN", 'Yfh_bxI94WD5YF3hcWwIzna10gcJwUx1aVR7AIhP9zI')
 }
 
 shiki_url = "https://shikimori.one/"
