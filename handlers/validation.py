484366011a7c7ea2f95443710d91805bc1583c36
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-03-05 12:38:53 +0300

    add check_user

--- handlers/validation.py
+++ handlers/validation.py
@@ -1,5 +1,5 @@
 import aiohttp
-
+from bot import dp, db_client
 from constants import headers
 
 
@@ -11,3 +11,16 @@ async def check_anime_title(title):
             if anime_founds:
                 return anime_founds[0]
     return None
+
+
+async def check_user_in_database(chat_id: int) -> bool:
+    # DB actions
+    db_current = db_client['telegram-shiki-bot']
+    collection = db_current["ids_users"]
+
+    if collection.find_one({'chat_id': chat_id}):
+        return True
+
+    await dp.bot.send_message(chat_id, 'You need to call command /MyProfile and link your nickname')
+    return False
+
