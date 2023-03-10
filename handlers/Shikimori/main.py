49069bef64eea67f99c2779f4672854cb8b2f25c
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-03-10 12:56:35 +0300

    rename files

--- handlers/Shikimori/main.py
+++ handlers/Shikimori/main.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
-from .shikimori_anime_handlers import register_anime_handlers
-from .shikimori_handlers import register_profile_handlers
+from .shikimori_anime import register_anime_handlers
+from .shikimori_profile import register_profile_handlers
 
 
 def register_shiki_handlers(dp: Dispatcher):
