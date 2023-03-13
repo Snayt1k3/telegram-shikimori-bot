63be37d4edd829f78fd09489357dd268976a2b99
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-03-13 14:18:18 +0300

    ref

--- handlers/Anilibria/notifications.py
+++ handlers/Anilibria/notifications.py
@@ -15,25 +15,29 @@ async def follow_notification(id_title: int, message: types.Message):
                                'animes': []})
 
     record = collection.find_one({'chat_id': message.chat.id})
-
     ani_l = record['animes'] if record['animes'] else []
-    collection.update_one({'chat_id': message.chat.id}, {'$set': {'animes': ani_l.append(id_title)}})
+
+    # get info for pretty message
     anime_info = await get_anime_info(id_title)
 
-    await message.answer(f"Вы подписались на Аниме - {anime_info['name']['ru']}")
+    if id_title in ani_l:
+        await message.answer(f"Вы Уже подписаны на Аниме - {anime_info['names']['ru']}")
+        return
+
+    collection.update_one({'chat_id': message.chat.id}, {'$set': {'animes': ani_l.append(id_title)}})
+
+    await message.answer(f"Вы подписались на Аниме - {anime_info['names']['ru']}")
 
 
 async def unfollow_notification(id_title: int, message: types.Message):
     # db
     db_current = db_client['telegram-shiki-bot']
     collection = db_current['user_follows']
-
+    record = collection.find_one({'chat_id': message.chat.id})
     # check user follow anime exists
-    if not collection.find_one({'chat_id': message.chat.id}):
+    if not record['animes']:
         await message.answer('У вас нету ни одного Аниме в подписках')
         return
-
-    record = collection.find_one({'chat_id': message.chat.id})
 
     collection.update_one({'chat_id': message.chat.id}, {'$set': {'animes': record['animes'].remove(id_title)}})
     anime_info = await get_anime_info(id_title)
