5ec34e872ce9151306cb398643f957d01e349631
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-07-29 15:14:30 +0300

    logic into db class

--- handlers/Anilibria/notifications.py
+++ handlers/Anilibria/notifications.py
@@ -1,65 +1,60 @@
+import logging
+
 from aiogram import types
 from anilibria import PlaylistUpdate, TitleEpisode
 
 from bot import dp, anilibria_client
-from database.database import DataBase
+from database.animedb import AnimeDB
 from misc.constants import ANI_URL
 
 
-async def follow_notification(id_title: int, message: types.Message):
-    """This method add id_title from anilibria.tv, into db"""
+async def follow_notification(title_id: int, message: types.Message) -> None:
+    """
+    This method, calling db method for add anime subscribe into db
+    :param title_id: Anilibria title_id
+    :param message: Telegram message
+    """
 
-    # check user follow anime exists
-    if not await DataBase.find_one('chat_id', message.chat.id, 'user_follows'):
-        await DataBase.insert_into_collection('user_follows', {'chat_id': message.chat.id, 'animes': []})
+    anime = await AnimeDB.subscribe_notifications(title_id, message.chat.id)
 
-    # validation animes into record
-    record = await DataBase.find_one('chat_id', message.chat.id, 'user_follows')
-    ls = record['animes'] if record['animes'] is not None else []
-
-    # get info for pretty message
-    anime_info = await anilibria_client.get_title(id=id_title)
-
-    if id_title in ls:
-        await message.answer(f"Вы уже подписаны на аниме - <b>{anime_info.names.ru}</b>")
-        return
-
-    # update user follows
-    ls.append(id_title)
-    await DataBase.update_one('user_follows', 'chat_id', message.chat.id, {'animes': ls})
-
-    await message.answer(f"Вы подписались на аниме - <b>{anime_info.names.ru}</b>")
+    if anime is None:
+        await message.answer("Произошла ошибка, попробуйте еще раз")
+    elif isinstance(anime, str):
+        await message.answer(anime)
+    else:
+        await message.answer(f"Вы успешно подписались на аниме - <i>{anime.title_ru}</i>")
 
 
-async def unfollow_notification(id_title: int, message: types.Message):
-    """This method delete id_title from db"""
-    record = DataBase.find_one('chat_id', message.chat.id, 'user_follows')
+async def unfollow_notification(title_id: int, message: types.Message) -> None:
+    """
+    This method, calling db method for delete anime subscribe from db
+    :param title_id: Anilibria title id
+    :param message: Telegram msg
+    """
+    anime = await AnimeDB.unsubscribe_notifications(title_id, message.chat.id)
 
-    # check user follow anime exists
-    if not record['animes']:
-        await message.answer('У вас нету ни одного аниме в подписках')
-        return
-
-    anime_info = await anilibria_client.get_title(id=id_title)
-    animes = list(map(int, record['animes']))
-    animes.remove(id_title)
-    await DataBase.update_one('user_follows', 'chat_id', message.chat.id, {'animes': animes})
-
-    await message.answer(f"Вы отписались от аниме - <b>{anime_info.names.ru}</b>")
+    if anime is None:
+        await message.answer("Произошла ошибка, попробуйте еще раз")
+    else:
+        await message.answer(f"Вы успешно отписались от аниме - <i> {anime.title_ru} </i>")
 
 
 @anilibria_client.on(TitleEpisode)
 async def send_notification(event: TitleEpisode):
     """Responsible for the delivery of notifications"""
-    # Get users follows
-    all_users = await DataBase.find('user_follows')
-    # iteration and check anime in users follows
-    for user in all_users:
-        if event.title.id in [int(i) for i in user['animes']]:
-            await dp.bot.send_photo(user['chat_id'], f"{event.title.posters.small.full_url}",
-                                    caption=f"<i>Вышла Новая Серия</i>"
-                                            f"<b>— {event.title.names.ru} | {event.title.names.en}</b>\n"
-                                            f"<b>Серия {event.episode.episode}</b>\n\n"
-                                            f"<b>Жанры</b>: {', '.join(event.title.genres)}\n"
-                                            f"<b>Озвучили</b>: {', '.join(event.title.team.voice)}",
-                                    )
+    try:
+        # Get all users
+        all_users = await AnimeDB.get_all_follows()
+
+        # iteration and check anime in users follows
+        for user in all_users:
+            if event.title.id in [i.id for i in user.follows]:
+                await dp.bot.send_photo(user.chat_id, f"{event.title.posters.small.full_url}",
+                                        caption=f"<i>Вышла Новая Серия</i>"
+                                                f"<b>— {event.title.names.ru} | {event.title.names.en}</b>\n"
+                                                f"<i>Серия {event.episode.episode}</i>\n\n"
+                                                f"<b>Жанры</b>: {', '.join(event.title.genres)}\n"
+                                                f"<b>Озвучили</b>: {', '.join(event.title.team.voice)}",
+                                        )
+    except Exception as e:
+        logging.error(f"Error occurred when trying to send notifications - {e}")
