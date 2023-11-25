f0bb5c7621d9db5b7517fe8003c9f1fdaf3d7837
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-11-25 14:38:13 +0300

    dotenv

--- bot.py
+++ bot.py
@@ -6,22 +6,21 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from anilibria import AniLibriaClient
 
-# for local start
-# from dotenv import load_dotenv
-# load_dotenv('./misc/.env')
+from dotenv import load_dotenv
+
+load_dotenv("./misc/.env")
 
 API_TOKEN = os.environ.get("TOKEN")
 
 # Configure logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s [%(levelname)s] %(message)s',  # msg form
-                    handlers=[
-                        logging.StreamHandler(),  # console
-                        logging.FileHandler("bot.log")
-                    ])
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",  # msg form
+    handlers=[logging.StreamHandler(), logging.FileHandler("bot.log")],  # console
+)
 
 # Initialize bot, dispatcher and other
 storage = MemoryStorage()
-bot = Bot(token=API_TOKEN, parse_mode='html')
+bot = Bot(token=API_TOKEN, parse_mode="html")
 dp = Dispatcher(bot, storage=storage)
 anilibria_client = AniLibriaClient()
