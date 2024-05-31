86ffe8df92d415a6038a2cbf0567e480ef32c3fe
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-05-31 19:28:02 +0300

    impl menu handler

--- src/presentation/telegram/common/keyboards/menu.py
+++ src/presentation/telegram/common/keyboards/menu.py
@@ -2,7 +2,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
-
 def main_menu() -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
     builder.row(
--- src/presentation/telegram/handlers/general/menu.py
+++ src/presentation/telegram/handlers/general/menu.py
@@ -2,8 +2,9 @@ from aiogram import types
 from aiogram.filters import Command
 
 from src.presentation.telegram.handlers.general.router import general
-
+from src.presentation.telegram.common.keyboards import main_menu
 
 @general.message(Command("menu"))
-async def main_menu(msg: types.Message):
-    pass
+async def menu(msg: types.Message):
+    await msg.reply(text="Ok", reply_markup=main_menu())
+
