d37207882aba9c3116b64e71835f4a14e4f9d411
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-06-07 16:30:03 +0300

    add interactor_factory.py

---
+++ src/presentation/telegram/interactor_factory.py
@@ -0,0 +1,79 @@
+from abc import abstractmethod, ABC
+from typing import AsyncContextManager
+
+from src.application.usecases.anime import (
+    AnilibriaSearchUseCase,
+    GetTorrentUseCase,
+    ShikimoriSearchUseCase,
+    GetUserListUseCase,
+)
+from src.application.usecases.user import (
+    AddUserUseCase,
+    DeleteUserUseCase,
+    AddFollowUseCase,
+    DeleteFollowUseCase,
+    GetAllFollowsUseCase,
+    CreateUserRateUseCase,
+    DeleteUserRateUseCase,
+    GetAllUserRates,
+    UpdateUserRateUseCase,
+    GetCredentialsUseCase,
+)
+
+
+class InteractorFactory(ABC):
+    @abstractmethod
+    def add_user(self) -> AsyncContextManager[AddUserUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_user(self) -> AsyncContextManager[DeleteUserUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_follow(self) -> AsyncContextManager[AddFollowUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def remove_follow(self) -> AsyncContextManager[DeleteFollowUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def all_follows(self) -> AsyncContextManager[GetAllFollowsUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_user_rate(self) -> AsyncContextManager[CreateUserRateUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_user_rate(self) -> AsyncContextManager[DeleteUserRateUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_user_rates(self) -> AsyncContextManager[GetAllUserRates]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_user_rate(self) -> AsyncContextManager[UpdateUserRateUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_credentials(self) -> AsyncContextManager[GetCredentialsUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def anilibria_search(self) -> AsyncContextManager[AnilibriaSearchUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def anilibria_get_torrent(self) -> AsyncContextManager[GetTorrentUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def shikimori_search(self) -> AsyncContextManager[ShikimoriSearchUseCase]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def shikimori_get_list(self) -> AsyncContextManager[GetUserListUseCase]:
+        raise NotImplementedError
