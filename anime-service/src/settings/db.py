27d560734e6e097333990d146041c8ffd61c176d
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-12-30 10:22:58 +0300

    add alembic to anime service

--- anime-service/src/settings/db.py
+++ anime-service/src/settings/db.py
@@ -1,15 +1,21 @@
+from dotenv import load_dotenv
+from pydantic import Field
+
 from src.settings.base import Settings
+
+load_dotenv()
 
 
 class DBSettings(Settings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    POSTGRES_HOST: str
+    POSTGRES_USER: str = Field()
+    POSTGRES_PASSWORD: str = Field()
+    POSTGRES_DB: str = Field()
+    POSTGRES_HOST: str = Field()
+    POSTGRES_PORT: str = Field()
 
     @property
     def url(self) -> str:
-        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:5432/{self.POSTGRES_DB}"
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 
 db_settings = DBSettings()
